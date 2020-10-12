025366e899e4169e1fedf532c25214d3b34b63f4
la5chine <khachinechaker@gmail.com>
2020-10-12 16:01:29 +0100

    class-based view

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('', views.PostList.as_view(template_name='blog/post_list.html'), name='post_list'),
+    path('post/<int:pk>/', views.PostDetail.as_view(template_name='blog/post_detail.html'), name='post_detail'),
+    path('post/new/', views.PostEdit.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.PostEdit.as_view(), name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,20 @@
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
-from .forms import PostForm
-from django.shortcuts import redirect
+from django.views import View
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
+from .forms import PostForm
+from .models import Post
 
 def post_list(request):
     posts = Post.objects.all().order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-
 
 def post_new(request):
     if request.method == "POST":
@@ -28,7 +29,6 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -42,3 +42,31 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+class PostList(ListView):
+    model = Post
+    context_object_name = 'posts'
+
+class PostDetail(DetailView):
+    def get_queryset(self):
+        return Post.objects.filter(pk=self.kwargs.get('pk'))
+
+class PostEdit(View):
+    template_name = 'blog/post_edit.html'
+
+    def get(self, request, *args, **kwargs):
+        if 'pk' in self.kwargs:
+            post = Post.objects.get(pk=self.kwargs["pk"])
+            form = PostForm(instance=post)
+        else:
+            form = PostForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request,*args, **kwargs):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
